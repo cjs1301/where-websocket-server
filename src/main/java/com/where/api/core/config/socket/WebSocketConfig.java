8c6b9502346399d8b08cc5948dcf50cb7656ede8
jeasong <cjs5241@gmail.com>
2023-12-14 23:35:44 +0900

    api gateway test 4

--- src/main/java/com/where/api/core/config/socket/WebSocketConfig.java
+++ src/main/java/com/where/api/core/config/socket/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("*");
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
