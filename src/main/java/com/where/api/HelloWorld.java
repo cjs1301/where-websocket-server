b4cd9d8ae4acb1acbb22183e0073868220284126
jeasong <cjs5241@gmail.com>
2023-12-14 18:35:23 +0900

    api gateway test 1

--- src/main/java/com/where/api/HelloWorld.java
+++ src/main/java/com/where/api/HelloWorld.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String test(){
         return "HelloWorld";
     }
