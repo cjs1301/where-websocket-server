3abb2fdbbb837a93d18dbfa0128c3d2027f1f619
jeasong <cjs5241@gmail.com>
2023-12-14 20:13:08 +0900

    api gateway test 2

--- src/main/java/com/where/api/StreamLambdaHandler.java
+++ src/main/java/com/where/api/StreamLambdaHandler.java
@@ -7,11 +7,12 @@ import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
+@Slf4j
 public class StreamLambdaHandler implements RequestStreamHandler {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
     static {
@@ -28,6 +29,9 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     }
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context)throws IOException{
+        log.info("InputStream",input);
+        log.info("OutputStream",output);
+        log.info("Context",context);
         handler.proxyStream(input,output,context);
     }
 }
